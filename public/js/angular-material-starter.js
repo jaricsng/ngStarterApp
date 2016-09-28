37fc57ba204ab25629e3227ca1c4fa0c6d991a59
Jaric Sng <jaric.sng@gmail.com>
2016-09-28 18:06:05 +0800

    fix loading svg error when app is run locally

--- public/js/angular-material-starter.js
+++ public/js/angular-material-starter.js
@@ -199,24 +199,24 @@ app.config(function($mdThemingProvider) {
 app.config(function($mdIconProvider) {
     $mdIconProvider
     // linking to https://github.com/google/material-design-icons/tree/master/sprites/svg-sprite
-    // 
-        .iconSet('action', 'https://raw.githubusercontent.com/google/material-design-icons/master/sprites/svg-sprite/svg-sprite-action.svg', 24)
-        .iconSet('alert', 'https://raw.githubusercontent.com/google/material-design-icons/master/sprites/svg-sprite/svg-sprite-alert.svg', 24)
-        .iconSet('av', 'https://raw.githubusercontent.com/google/material-design-icons/master/sprites/svg-sprite/svg-sprite-av.svg', 24)
-        .iconSet('communication', 'https://raw.githubusercontent.com/google/material-design-icons/master/sprites/svg-sprite/svg-sprite-communication.svg', 24)
-        .iconSet('content', 'https://raw.githubusercontent.com/google/material-design-icons/master/sprites/svg-sprite/svg-sprite-content.svg', 24)
-        .iconSet('device', 'https://raw.githubusercontent.com/google/material-design-icons/master/sprites/svg-sprite/svg-sprite-device.svg', 24)
-        .iconSet('editor', 'https://raw.githubusercontent.com/google/material-design-icons/master/sprites/svg-sprite/svg-sprite-editor.svg', 24)
-        .iconSet('file', 'https://raw.githubusercontent.com/google/material-design-icons/master/sprites/svg-sprite/svg-sprite-file.svg', 24)
-        .iconSet('hardware', 'https://raw.githubusercontent.com/google/material-design-icons/master/sprites/svg-sprite/svg-sprite-hardware.svg', 24)
-        .iconSet('image', 'https://raw.githubusercontent.com/google/material-design-icons/master/sprites/svg-sprite/svg-sprite-image.svg', 24)
-        .iconSet('maps', 'https://raw.githubusercontent.com/google/material-design-icons/master/sprites/svg-sprite/svg-sprite-maps.svg', 24)
-        .iconSet('navigation', 'https://raw.githubusercontent.com/google/material-design-icons/master/sprites/svg-sprite/svg-sprite-navigation.svg', 24)
-        .iconSet('notification', 'https://raw.githubusercontent.com/google/material-design-icons/master/sprites/svg-sprite/svg-sprite-notification.svg', 24)
-        .iconSet('social', 'https://raw.githubusercontent.com/google/material-design-icons/master/sprites/svg-sprite/svg-sprite-social.svg', 24)
-        .iconSet('toggle', 'https://raw.githubusercontent.com/google/material-design-icons/master/sprites/svg-sprite/svg-sprite-toggle.svg', 24)
+    //
+        .iconSet('action', 'img/svg-sprite-action.svg', 24)
+        .iconSet('alert', 'img/svg-sprite-alert.svg', 24)
+        .iconSet('av', 'img/svg-sprite-av.svg', 24)
+        .iconSet('communication', 'img/svg-sprite-communication.svg', 24)
+        .iconSet('content', 'img/svg-sprite-content.svg', 24)
+        .iconSet('device', 'img/svg-sprite-device.svg', 24)
+        .iconSet('editor', 'img/svg-sprite-editor.svg', 24)
+        .iconSet('file', 'img/svg-sprite-file.svg', 24)
+        .iconSet('hardware', 'img/svg-sprite-hardware.svg', 24)
+        .iconSet('image', 'img/svg-sprite-image.svg', 24)
+        .iconSet('maps', 'img/svg-sprite-maps.svg', 24)
+        .iconSet('navigation', 'img/svg-sprite-navigation.svg', 24)
+        .iconSet('notification', 'img/svg-sprite-notification.svg', 24)
+        .iconSet('social', 'img/svg-sprite-social.svg', 24)
+        .iconSet('toggle', 'img/svg-sprite-toggle.svg', 24)
 
     // Illustrated user icons used in the docs https://material.angularjs.org/latest/#/demo/material.components.gridList
-    .iconSet('avatars', 'https://raw.githubusercontent.com/angular/material/master/docs/app/icons/avatar-icons.svg', 24)
-        .defaultIconSet('https://raw.githubusercontent.com/google/material-design-icons/master/sprites/svg-sprite/svg-sprite-action.svg', 24);
+    .iconSet('avatars', 'img/avatar-icons.svg', 24)
+        .defaultIconSet('img/svg-sprite-action.svg', 24);
 });
